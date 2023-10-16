940dd09e731072bb0c2d0cd4caa8e7c8182e48cc
FibreCase Xu <fibrecase@outlook.com>
2023-10-16 21:39:02 +0800

    happy!

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -111,3 +111,249 @@
 //	}
 //	return z;//输出的即为对xx取后的结果
 //}
+
+
+//*****************************************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	float m, n, count;
+//	scanf("%f,%f", &m, &n);
+//	if (200 < m && m < 300 && n>0 && n < 100)
+//		count = 0.0 + m * n / 100;
+//	printf("%d\n", (int)ceil(count));
+//	return 0;
+//}
+//
+//****************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	float m, n, x, count;
+//	scanf("%f%f", &m, &n);
+//	if (200 < m && m < 300 && n>0 && n < 100)
+//		x = m * n / 1000;
+//	count = round(x)*10;
+//	printf("%d\n", (int)count);
+//	return 0;
+//}
+//
+//*********************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	float y1, j1 ,f1 ,y2,j2,f2 ,rst ;
+//	scanf("%f%f%f", &y1, &j1, &f1);
+//	scanf("%f%f%f", &y2, &j2, &f2);
+//	rst = y1 + y2 + j1 * 0.1 + j2 * 0.1 + f1 * 0.01 + f2 * 0.01;
+//	printf("%.2f", rst);
+//}
+
+//************************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//void main()
+//{
+//	int roundff(float x);
+//	int t;
+//	float x;
+//	x = 1.8;
+//
+//	t = roundff(x);
+//	printf("%d", t);
+//}
+//
+//int roundff(float x)
+//{
+//	float y;
+//	int z;
+//	y = (int)(x * 10) % 10;
+//	if (y >= 5)
+//	{
+//		z = ceil(x);
+//	}
+//	else
+//	{
+//		z = floor(x);
+//	}
+//	printf("%d", z);
+//	return z;
+//}
+
+
+//**********************************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//int main()
+//{
+//	float m, n, x, y, z, a;
+//	int rst;
+//	int roundff(float x);
+//	scanf("%f%f", &m, &n);
+//	if (m > 200 && m < 300 && n>0 && n < 100)
+//	{
+//		x = n / 1000 * m;//此处的1000是为了和后面的*10呼应
+//	}
+//	//由于是取十位，而roundff取的是个位，所以先/10
+//	rst = roundff(x);
+//	printf("%d\n", rst * 10);
+//	return 0;
+//}
+//
+//int roundff(float xx)
+//{
+//	float y;
+//	int z;
+//	y = (int)(xx * 10) % 10;
+//	if (y >= 5)
+//	{
+//		z = ceil(xx);
+//	}
+//	else
+//	{
+//		z = floor(xx);
+//	}
+//	return z;//输出的即为对xx取后的结果
+//}
+
+//**************************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (a % 2 == 0)
+//	{
+//		printf("%d是个偶数", a);
+//	}
+//	else
+//	{
+//		printf("%d是个奇数", a);
+//	}
+//	return 0;
+//}
+
+//***********************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (a % 100 == 0)
+//	{
+//		if (a % 400 == 0)
+//		{
+//			printf("%d年是闰年", a);
+//		}
+//		else
+//		{
+//			printf("%d年不是闰年", a);
+//		}
+//	}
+//	else
+//	{
+//		if (a % 4 == 0)
+//		{
+//			printf("%d年是闰年", a);
+//		}
+//		else
+//		{
+//			printf("%d年不是闰年", a);
+//		}
+//	}
+//	return 0;
+//}
+
+//***************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int a, b, c, m;
+//	scanf("%d,%d,%d", &a, &b, &c);
+//	m = a;
+//	if (b >= m)
+//		m = b;
+//	if (c >= m)
+//		m = c;
+//	printf("最大数是%d", m);
+//	return 0;
+//}
+
+
+//*******************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	int a, b, c;
+//	float s1, s2, p;
+//	scanf("%d%d%d", &a, &b, &c);
+//
+//	if ((a + b) > c && (a + c) > b && (b + c) > a)
+//	{
+//		p = ((float)a + b + c) / 2;
+//		s1 = p * (p - a) * (p - b) * (p - c);
+//		s2 = sqrt(s1);
+//		printf("%.2f", s2);
+//	}
+//	else
+//	{
+//		printf("不是三角形");
+//	}
+//
+//	return 0;
+//}
+
+
+
+//**********************************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	int i, t;
+//	int ss[4] = { 0 };
+//
+//	//to input every number
+//
+//	scanf("%d,%d,%d,%d", &ss[0], &ss[1], &ss[2], &ss[3]);
+//
+//	//to order the number
+//	for (i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j < 3; j++)
+//		{
+//			if (ss[j] > ss[j + 1])
+//			{
+//				t = ss[j];
+//				ss[j] = ss[j + 1];
+//				ss[j + 1] = t;
+//			} 
+//		}
+//	}
+//
+//	printf("排序结果如下:%d,%d,%d,%d", ss[0], ss[1], ss[2], ss[3]);
+//	return 0;
+//}
