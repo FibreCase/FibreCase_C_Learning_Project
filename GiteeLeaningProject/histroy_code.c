035622086186cc70d76883f31efe9dfcfa874ce4
FibreCase Xu <fibrecase@outlook.com>
2023-10-17 22:52:54 +0800

    push today

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -357,3 +357,29 @@
 //	printf("排序结果如下:%d,%d,%d,%d", ss[0], ss[1], ss[2], ss[3]);
 //	return 0;
 //}
+
+//**************************************************************
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include<math.h>
+//int main()
+//{
+//    float a, b, c;
+//    scanf("%f,%f,%f", &a, &b, &c);
+//    if (a + b > c && b + c > a && c + a > b)
+//    {
+//        if (fabs(a * a + b * b - c * c) <= 0.000001 && a != b || fabs(a * a + c * c - b * b) <= 0.000001 && a != c || fabs(b * b + c * c - a * a) <= 0.000001 && b != c)
+//            printf("直角三角形");
+//        else if (fabs(a - b) <= 0.000001 && fabs(b - c) <= 0.000001 && fabs(a - c) <= 0.000001)
+//            printf("等边三角形");
+//        else if (fabs(a - b) <= 0.000001 && a != c && (a * a + b * b) != c * c || fabs(b - c) <= 0.000001 && b != a && (b * b + c * c) != a * a || fabs(a - c) <= 0.000001 && a != b && (a * a + c * c) != b * b)
+//            printf("等腰三角形");
+//        else if (fabs(a - b) <= 0.000001 && a != c && fabs(a * a + b * b - c * c) <= 0.000001 || fabs(b - c) <= 0.000001 && b != a && fabs(b * b + c * c - a * a) <= 0.000001 || fabs(a - c) <= 0.000001 && a != b && fabs(a * a + c * c - b * b) <= 0.000001)
+//            printf("等腰直角三角形");
+//        else
+//            printf("一般三角形");
+//    }
+//    else
+//        printf("不是三角形");
+//    return 0;
+//}
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -1,13 +1,24 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
-
+#include<math.h>
 int main()
 {
-	float m, n, count;
-	scanf("%f,%f", &m, &n);
-	if (200 < m && m < 300 && n>0 && n < 100)
-		count = 0.0 + m * n / 100;
-	printf("%d\n", (int)ceil(count));
-	return 0;
+    float a, b, c;
+    scanf("%f,%f,%f", &a, &b, &c);
+    if (a + b > c && b + c > a && c + a > b)
+    {
+        if (fabs(a * a + b * b - c * c) <= 0.000001 && a != b || fabs(a * a + c * c - b * b) <= 0.000001 && a != c || fabs(b * b + c * c - a * a) <= 0.000001 && b != c)
+            printf("直角三角形");
+        else if (fabs(a - b) <= 0.000001 && fabs(b - c) <= 0.000001 && fabs(a - c) <= 0.000001)
+            printf("等边三角形");
+        else if (fabs(a - b) <= 0.000001 && a != c && (a * a + b * b) != c * c || fabs(b - c) <= 0.000001 && b != a && (b * b + c * c) != a * a || fabs(a - c) <= 0.000001 && a != b && (a * a + c * c) != b * b)
+            printf("等腰三角形");
+        else if (fabs(a - b) <= 0.000001 && a != c && fabs(a * a + b * b - c * c) <= 0.000001 || fabs(b - c) <= 0.000001 && b != a && fabs(b * b + c * c - a * a) <= 0.000001 || fabs(a - c) <= 0.000001 && a != b && fabs(a * a + c * c - b * b) <= 0.000001)
+            printf("等腰直角三角形");
+        else
+            printf("一般三角形");
+    }
+    else
+        printf("不是三角形");
+    return 0;
 }
