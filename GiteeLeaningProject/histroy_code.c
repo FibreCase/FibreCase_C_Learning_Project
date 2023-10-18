67e94ee55c30cd55486d9fcd814a83b6527e728c
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 15:17:53 +0800

    4.1.1

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -547,3 +547,32 @@
 //    return 0;
 //}
 //
+
+//***********************************************
+//4.1.1
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//
+//int main()
+//{
+//    char c; //用户输入字符
+//    int letters = 0, // 字母数目
+//        space = 0, // 空格数目
+//        digit = 0, // 整数数目
+//        others = 0; // 其他字符数目
+//    while ((c = getchar()) != '\n')
+//    { // 每次读取一个字符，回车时结束
+//        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+//            letters++;//字母＋1
+//        else if (c == ' ')
+//            space++;//空格+1
+//        else if (c >= '0' && c <= '9')
+//            digit++;//数字+1
+//        else
+//            others++;//其他+1
+//    }
+//    printf("字母数:%d\n空格数:%d\n数字数:%d\n其它字符数:%d\n\n", letters, space, digit, others);
+//
+//    return 0;
+//}
+
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -1,61 +1,26 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 
-int main()
+int main() 
 {
-    float mon=0,inter;
-    char i;
-    scanf("%f", &inter);
+    char c; //用户输入字符
+    int letters = 0, // 字母数目
+        space = 0, // 空格数目
+        digit = 0, // 整数数目
+        others = 0; // 其他字符数目
+    while ((c = getchar()) != '\n') 
+    { // 每次读取一个字符，回车时结束
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+            letters++;//字母＋1
+        else if (c == ' ')
+            space++;//空格+1
+        else if (c >= '0' && c <= '9')
+            digit++;//数字+1
+        else
+            others++;//其他+1
+    }
+    printf("字母数:%d\n空格数:%d\n数字数:%d\n其它字符数:%d\n\n", letters, space, digit, others);
 
-    if (inter < 100000)
-    {
-        i = 1;
-    }
-    else if (inter <= 200000 && inter > 100000)
-    {
-        i = 2;
-    }
-    else if (inter <= 400000 && inter > 200000)
-    {
-        i = 3;
-    }
-    else if (inter <= 600000 && inter > 400000)
-    {
-        i = 4;
-    }
-    else if (inter <= 1000000 && inter > 600000)
-    {
-        i = 5;
-    }
-    else if (inter > 1000000)
-    {
-        i = 6;
-    }
-
-    switch (i)
-    {
-    
-    case 6:
-        mon = (inter - 1e6) * 0.01;
-        inter = 1e6;
-    case 5:
-        mon = (inter - 6e5) * 0.015 + mon;
-        inter = 6e5;
-    case 4:
-        mon = (inter - 4e5) * 0.03 + mon;
-        inter = 4e5;
-    case 3:
-        mon = (inter - 2e5) * 0.05 + mon;
-        inter = 2e5;
-    case 2:
-        mon = (inter - 1e5) * 0.075 + mon;
-        inter = 1e5;
-    case 1:
-        mon = inter * 0.1 + mon;
-    }
-
-    printf("奖金是%.2f", mon);
     return 0;
 }
     
