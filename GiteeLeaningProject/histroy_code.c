85f888218d6f513337bd654b7e69a832a99e4f57
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 16:45:49 +0800

    order update

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -1,3 +1,113 @@
+//****************************************************************
+//4.1.3
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a = 1, b = 0, c = 0;
+	int verf(int, int, int);
+
+	while (a < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				verf(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	return 0;
+}
+
+int verf(int a, int b, int c)
+{
+	int n, v;
+	n = a * 100 + b * 10 + c;
+	v = a * a * a + b * b * b + c * c * c;
+	if (n == v)
+	{
+		printf("%5d", n);
+	}
+
+	return 0;
+}
+
+
+//****************************************************
+//4.1.2
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int m, n, i, a;
+	int min(int, int), max(int, int);
+	scanf("%d,%d", &m, &n);
+	i = min(m, n);
+	while (i >= 1)
+	{
+		if (m % i == 0 && n % i == 0)
+		{
+			printf("最大公约数是%d\n", i);
+			break;
+		}
+		else
+		{
+			i--;
+		}
+	}
+	a = max(m, n);
+	while (1)
+	{
+		if (a % m == 0 && a % n == 0)
+		{
+			printf("最小公倍数是%d", a);
+			break;
+		}
+		else
+		{
+			a++;
+		}
+	}
+
+
+	return 0;
+}
+
+int min(int m, int n)
+{
+	if (m >= n)
+	{
+		return n;
+	}
+	else
+	{
+		return m;
+	}
+}
+
+int max(int m, int n)
+{
+	if (m >= n)
+	{
+		return m;
+	}
+	else
+	{
+		return n;
+	}
+}
+*****************************************************
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
@@ -75,8 +185,208 @@ int roundff(float x)
 	return z;
 }
 
+//***********************************************
+//4.1.1
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
-**********************************************************************
+int main()
+{
+	char c; //用户输入字符
+	int letters = 0, // 字母数目
+		space = 0, // 空格数目
+		digit = 0, // 整数数目
+		others = 0; // 其他字符数目
+	while ((c = getchar()) != '\n')
+	{ // 每次读取一个字符，回车时结束
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			letters++;//字母＋1
+		else if (c == ' ')
+			space++;//空格+1
+		else if (c >= '0' && c <= '9')
+			digit++;//数字+1
+		else
+			others++;//其他+1
+	}
+	printf("字母数:%d\n空格数:%d\n数字数:%d\n其它字符数:%d\n\n", letters, space, digit, others);
+
+	return 0;
+}
+
+//*******************************************************************
+//3.2.4
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float mon = 0, inter;
+	char i;
+	scanf("%f", &inter);
+
+	if (inter < 100000)
+	{
+		i = 1;
+	}
+	else if (inter <= 200000 && inter > 100000)
+	{
+		i = 2;
+	}
+	else if (inter <= 400000 && inter > 200000)
+	{
+		i = 3;
+	}
+	else if (inter <= 600000 && inter > 400000)
+	{
+		i = 4;
+	}
+	else if (inter <= 1000000 && inter > 600000)
+	{
+		i = 5;
+	}
+	else if (inter > 1000000)
+	{
+		i = 6;
+	}
+
+	switch (i)
+	{
+
+	case 6:
+		mon = (inter - 1e6) * 0.01;
+		inter = 1e6;
+	case 5:
+		mon = (inter - 6e5) * 0.015 + mon;
+		inter = 6e5;
+	case 4:
+		mon = (inter - 4e5) * 0.03 + mon;
+		inter = 4e5;
+	case 3:
+		mon = (inter - 2e5) * 0.05 + mon;
+		inter = 2e5;
+	case 2:
+		mon = (inter - 1e5) * 0.075 + mon;
+		inter = 1e5;
+	case 1:
+		mon = inter * 0.1 + mon;
+	}
+
+	printf("奖金是%.2f", mon);
+	return 0;
+}
+
+//*****************************************************************
+//3.2.3
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float x, y;
+	scanf("%f,%f", &x, &y);
+	if (fabs(fabs(x) - 2) < 1 && fabs(fabs(y) - 2) < 1)
+	{
+		printf("该点高度为10米");
+	}
+	else
+	{
+		printf("该点高度为0米");
+	}
+
+	return 0;
+}
+
+//*****************************************************************************
+//3.2.2
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	float x, y;
+	scanf("%f%f", &x, &y);
+	if (x == 0 && y == 0)
+	{
+		printf("(%.2f,%.2f)是原点", x, y);
+	}
+	else if (x == 0)
+	{
+		printf("(%.2f,%.2f)在y坐标轴上", x, y);
+	}
+	else if (y == 0)
+	{
+		printf("(%.2f,%.2f)在x坐标轴上", x, y);
+	}
+	else if (x > 0 && y > 0)
+	{
+		printf("(%.2f,%.2f)在第一象限", x, y);
+	}
+	else if (x < 0 && y > 0)
+	{
+		printf("(%.2f,%.2f)在第二象限", x, y);
+	}
+	else if (x < 0 && y < 0)
+	{
+		printf("(%.2f,%.2f)在第三象限", x, y);
+	}
+	else if (x > 0 && y < 0)
+	{
+		printf("(%.2f,%.2f)在第四象限", x, y);
+	}
+
+	return 0;
+}
+
+//**********************************************************
+//3.2.1
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+	float grade;
+	scanf("%f", &grade);
+
+	if (grade <= 100 && grade >= 0)
+	{
+		switch ((int)grade / 10)
+		{
+		default:printf("成绩是%.1f,相应的等级是E", grade);
+			break;
+		case 10:
+		case 9:
+			printf("成绩是%.1f,相应的等级是A", grade); break;
+		case 8:
+			printf("成绩是%.1f,相应的等级是B", grade); break;
+		case 7:
+			printf("成绩是%.1f,相应的等级是C", grade); break;
+		case 6:
+			printf("成绩是%.1f,相应的等级是D", grade); break;
+		}
+	}
+	else
+	{
+		printf("输入成绩有误");
+	}
+	return 0;
+}
+
+
+
+
+
+
+//************************************************************************************************************************
+
+//3.2.1之前的作业在下面的历史代码中
+
+//************************************************************************************************************************
+
+//历史代码按时间正序排列
+
+//************************************************************************************************************************
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <math.h>
@@ -96,9 +406,8 @@ int main()
 	return 0;
 }
 
-int roundff(float xx)
-{
-	float y;
+in roundff(float xx)
+{	float y;
 	int z;
 	y = (int)(xx * 10) % 10;
 	if (y >= 5)
@@ -385,303 +694,4 @@ int main()
 }
 
 
-**********************************************************
-3.2.1
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include<math.h>
-int main()
-{
-    float grade;
-    scanf("%f", &grade);
 
-    if (grade <= 100 && grade >= 0)
-    {
-        switch ((int)grade / 10)
-        {
-        default:printf("成绩是%.1f,相应的等级是E", grade);
-            break;
-        case 10:
-        case 9:
-            printf("成绩是%.1f,相应的等级是A", grade); break;
-        case 8:
-            printf("成绩是%.1f,相应的等级是B", grade); break;
-        case 7:
-            printf("成绩是%.1f,相应的等级是C", grade); break;
-        case 6:
-            printf("成绩是%.1f,相应的等级是D", grade); break;
-        }
-    }
-    else
-    {
-        printf("输入成绩有误");
-    }
-    return 0;
-}
-
-*****************************************************************************
-3.2.2
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main()
-{
-    float x, y;
-    scanf("%f%f", &x, &y);
-    if (x == 0 && y == 0)
-    {
-        printf("(%.2f,%.2f)是原点", x, y);
-    }
-    else if (x == 0)
-    {
-        printf("(%.2f,%.2f)在y坐标轴上", x, y);
-    }
-    else if (y == 0)
-    {
-        printf("(%.2f,%.2f)在x坐标轴上", x, y);
-    }
-    else if (x > 0 && y > 0)
-    {
-        printf("(%.2f,%.2f)在第一象限", x, y);
-    }
-    else if (x < 0 && y > 0)
-    {
-        printf("(%.2f,%.2f)在第二象限", x, y);
-    }
-    else if (x < 0 && y < 0)
-    {
-        printf("(%.2f,%.2f)在第三象限", x, y);
-    }
-    else if (x > 0 && y < 0)
-    {
-        printf("(%.2f,%.2f)在第四象限", x, y);
-    }
- 
-	  return 0;
-}
-
-
-*****************************************************************
-3.2.3
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    float x, y;
-    scanf("%f,%f", &x, &y);
-    if (fabs(fabs(x) - 2) < 1 && fabs(fabs(y) - 2) < 1)
-    {
-        printf("该点高度为10米");
-    }
-    else
-    {
-        printf("该点高度为0米");
-    }
-
-    return 0;
-}
-
-
-*******************************************************************
-3.2.4
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    float mon = 0, inter;
-    char i;
-    scanf("%f", &inter);
-
-    if (inter < 100000)
-    {
-        i = 1;
-    }
-    else if (inter <= 200000 && inter > 100000)
-    {
-        i = 2;
-    }
-    else if (inter <= 400000 && inter > 200000)
-    {
-        i = 3;
-    }
-    else if (inter <= 600000 && inter > 400000)
-    {
-        i = 4;
-    }
-    else if (inter <= 1000000 && inter > 600000)
-    {
-        i = 5;
-    }
-    else if (inter > 1000000)
-    {
-        i = 6;
-    }
-
-    switch (i)
-    {
-
-    case 6:
-        mon = (inter - 1e6) * 0.01;
-        inter = 1e6;
-    case 5:
-        mon = (inter - 6e5) * 0.015 + mon;
-        inter = 6e5;
-    case 4:
-        mon = (inter - 4e5) * 0.03 + mon;
-        inter = 4e5;
-    case 3:
-        mon = (inter - 2e5) * 0.05 + mon;
-        inter = 2e5;
-    case 2:
-        mon = (inter - 1e5) * 0.075 + mon;
-        inter = 1e5;
-    case 1:
-        mon = inter * 0.1 + mon;
-    }
-
-    printf("奖金是%.2f", mon);
-    return 0;
-}
-
-
-***********************************************
-4.1.1
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-
-int main()
-{
-    char c; //用户输入字符
-    int letters = 0, // 字母数目
-        space = 0, // 空格数目
-        digit = 0, // 整数数目
-        others = 0; // 其他字符数目
-    while ((c = getchar()) != '\n')
-    { // 每次读取一个字符，回车时结束
-        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-            letters++;//字母＋1
-        else if (c == ' ')
-            space++;//空格+1
-        else if (c >= '0' && c <= '9')
-            digit++;//数字+1
-        else
-            others++;//其他+1
-    }
-    printf("字母数:%d\n空格数:%d\n数字数:%d\n其它字符数:%d\n\n", letters, space, digit, others);
-
-    return 0;
-}
-
-****************************************************
-4.1.2
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-    int m, n, i, a;
-    int min(int, int), max(int, int);
-    scanf("%d,%d", &m, &n);
-    i = min(m, n);
-    while (i >= 1)
-    {
-        if (m % i == 0 && n % i == 0)
-        {
-            printf("最大公约数是%d\n", i);
-            break;
-        }
-        else
-        {
-            i--;
-        }
-    }
-    a = max(m, n);
-    while (1)
-    {
-        if (a % m == 0 && a % n == 0)
-        {
-            printf("最小公倍数是%d", a);
-            break;
-        }
-        else
-        {
-            a++;
-        }
-    }
-
-
-    return 0;
-}
-
-int min(int m, int n)
-{
-    if (m >= n)
-    {
-        return n;
-    }
-    else
-    {
-        return m;
-    }
-}
-
-int max(int m, int n)
-{
-    if (m >= n)
-    {
-        return m;
-    }
-    else
-    {
-        return n;
-    }
-}
-
-
-****************************************************************
-4.1.3
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-	int a = 1, b = 0, c = 0;
-	int verf(int, int, int);
-
-	while (a < 10)
-	{
-		b = 0;
-		while (b < 10)
-		{
-			c = 0;
-			while (c < 10)
-			{
-				verf(a, b, c);
-				c++;
-			}
-			b++;
-		}
-		a++;
-	}
-
-return 0;
-}
-
-int verf(int a, int b, int c)
-{
-	int n, v;
-	n = a * 100 + b * 10 + c;
-	v = a * a * a + b * b * b + c * c * c;
-	if (n == v)
-	{
-		printf("%5d", n);
-	}
-
-	return 0;
-}
