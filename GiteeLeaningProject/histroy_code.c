e8db35061b60010fe6b79dbcb182343766f17c5e
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 14:00:31 +0800

    3.2.1

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -383,3 +383,38 @@
 //        printf("不是三角形");
 //    return 0;
 //}
+
+
+//**********************************************************
+//3.2.1
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include<math.h>
+//int main()
+//{
+//    float grade;
+//    scanf("%f", &grade);
+//
+//    if (grade <= 100 && grade >= 0)
+//    {
+//        switch ((int)grade / 10)
+//        {
+//        default:printf("成绩是%.1f,相应的等级是E", grade);
+//            break;
+//        case 10:
+//        case 9:
+//            printf("成绩是%.1f,相应的等级是A", grade); break;
+//        case 8:
+//            printf("成绩是%.1f,相应的等级是B", grade); break;
+//        case 7:
+//            printf("成绩是%.1f,相应的等级是C", grade); break;
+//        case 6:
+//            printf("成绩是%.1f,相应的等级是D", grade); break;
+//        }
+//    }
+//    else
+//    {
+//        printf("输入成绩有误");
+//    }
+//    return 0;
+//}
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -3,22 +3,29 @@
 #include<math.h>
 int main()
 {
-    float a, b, c;
-    scanf("%f,%f,%f", &a, &b, &c);
-    if (a + b > c && b + c > a && c + a > b)
+    float grade;
+    scanf("%f", &grade);
+    
+    if (grade <= 100 && grade >= 0)
     {
-        if (fabs(a * a + b * b - c * c) <= 0.000001 && a != b || fabs(a * a + c * c - b * b) <= 0.000001 && a != c || fabs(b * b + c * c - a * a) <= 0.000001 && b != c)
-            printf("直角三角形");
-        else if (fabs(a - b) <= 0.000001 && fabs(b - c) <= 0.000001 && fabs(a - c) <= 0.000001)
-            printf("等边三角形");
-        else if (fabs(a - b) <= 0.000001 && a != c && (a * a + b * b) != c * c || fabs(b - c) <= 0.000001 && b != a && (b * b + c * c) != a * a || fabs(a - c) <= 0.000001 && a != b && (a * a + c * c) != b * b)
-            printf("等腰三角形");
-        else if (fabs(a - b) <= 0.000001 && a != c && fabs(a * a + b * b - c * c) <= 0.000001 || fabs(b - c) <= 0.000001 && b != a && fabs(b * b + c * c - a * a) <= 0.000001 || fabs(a - c) <= 0.000001 && a != b && fabs(a * a + c * c - b * b) <= 0.000001)
-            printf("等腰直角三角形");
-        else
-            printf("一般三角形");
+        switch ((int)grade/10)
+        {
+        default:printf("成绩是%.1f,相应的等级是E",grade);
+            break;
+        case 10:
+        case 9:
+            printf("成绩是%.1f,相应的等级是A", grade); break;
+        case 8:
+            printf("成绩是%.1f,相应的等级是B", grade); break;
+        case 7:
+            printf("成绩是%.1f,相应的等级是C", grade); break;
+        case 6:
+            printf("成绩是%.1f,相应的等级是D", grade); break;
+        }
     }
     else
-        printf("不是三角形");
+    {
+        printf("输入成绩有误");
+    }
     return 0;
 }
