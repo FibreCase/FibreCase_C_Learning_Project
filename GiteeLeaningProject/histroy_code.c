50a5f675757840b2f18781f09f17dd380596a90f
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 20:28:08 +0800

    4.1.8

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -1,4 +1,24 @@
 //************************************************************************
+//4.1.8
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n, sum = 0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		sum = sum + i;
+	}
+
+	printf("1+2+...+%d=%d\n", n, sum);
+	return 0;
+}
+
+//************************************************************************
 //4.1.7
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -4,45 +4,16 @@
 
 int main() 
 {
-	int b = 0, c = 0,t;
-	int ver(int, int);
-	while (b<9&&c<9)
+	int n,sum=0;
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
 	{
-		for (int i = 0; i < 10;)
-		{
-			if (ver(b,c) == 1)
-			{
-				i++; c++;
-				if (c == 10)
-				{
-					b++; c = 0;
-				}
-	
-			}
-			else
-			{
-				c++;
-				if (c == 10)
-				{
-					b++; c = 0;
-				}
-			}
-		}
-		printf("\n");
+		sum = sum + i;
 	}
+
+	printf("1+2+...+%d=%d\n", n, sum);
     return 0;
 }
 
 
-int ver(int b, int c)
-{
-	if ((100+b*10+c)%2==0 && b!=c && b != 1 && c != 1)
-	{
-		printf("%6d", (100 + b * 10 + c));
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
