0d750aaf7b619e9a8cade31feeb363d03a44bd28
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 16:48:11 +0800

    bugs fixed

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -80,7 +80,6 @@ int main()
 		}
 	}
 
-
 	return 0;
 }
 
@@ -106,83 +105,6 @@ int max(int m, int n)
 	{
 		return n;
 	}
-}
-*****************************************************
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-	float m, n, count;
-	scanf("%f,%f", &m, &n);
-	if (200 < m && m < 300 && n>0 && n < 100)
-		count = 0.0 + m * n / 100;
-	printf("%d\n", (int)ceil(count));
-	return 0;
-}
-
-****************************************************
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-	float m, n, x, count;
-	scanf("%f%f", &m, &n);
-	if (200 < m && m < 300 && n>0 && n < 100)
-		x = m * n / 1000;
-	count = round(x)*10;
-	printf("%d\n", (int)count);
-	return 0;
-}
-
-*********************************************************
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-int main()
-{
-	float y1, j1 ,f1 ,y2,j2,f2 ,rst ;
-	scanf("%f%f%f", &y1, &j1, &f1);
-	scanf("%f%f%f", &y2, &j2, &f2);
-	rst = y1 + y2 + j1 * 0.1 + j2 * 0.1 + f1 * 0.01 + f2 * 0.01;
-	printf("%.2f", rst);
-}
-
-************************************************************
-#define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
-#include <math.h>
-
-void main()
-{
-	int roundff(float x);
-	int t;
-	float x;
-	x = 1.8;
-
-	t = roundff(x);
-	printf("%d", t);
-}
-
-int roundff(float x)
-{
-	float y;
-	int z;
-	y = (int)(x * 10) % 10;
-	if (y >= 5)
-	{
-		z = ceil(x);
-	}
-	else
-	{
-		z = floor(x);
-	}
-	printf("%d", z);
-	return z;
 }
 
 //***********************************************
@@ -691,6 +613,84 @@ int main()
     else
         printf("不是三角形");
     return 0;
+}
+
+*****************************************************
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float m, n, count;
+	scanf("%f,%f", &m, &n);
+	if (200 < m && m < 300 && n>0 && n < 100)
+		count = 0.0 + m * n / 100;
+	printf("%d\n", (int)ceil(count));
+	return 0;
+}
+
+****************************************************
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float m, n, x, count;
+	scanf("%f%f", &m, &n);
+	if (200 < m && m < 300 && n>0 && n < 100)
+		x = m * n / 1000;
+	count = round(x) * 10;
+	printf("%d\n", (int)count);
+	return 0;
+}
+
+*********************************************************
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float y1, j1, f1, y2, j2, f2, rst;
+	scanf("%f%f%f", &y1, &j1, &f1);
+	scanf("%f%f%f", &y2, &j2, &f2);
+	rst = y1 + y2 + j1 * 0.1 + j2 * 0.1 + f1 * 0.01 + f2 * 0.01;
+	printf("%.2f", rst);
+}
+
+************************************************************
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+	int roundff(float x);
+	int t;
+	float x;
+	x = 1.8;
+
+	t = roundff(x);
+	printf("%d", t);
+}
+
+int roundff(float x)
+{
+	float y;
+	int z;
+	y = (int)(x * 10) % 10;
+	if (y >= 5)
+	{
+		z = ceil(x);
+	}
+	else
+	{
+		z = floor(x);
+	}
+	printf("%d", z);
+	return z;
 }
 
 
