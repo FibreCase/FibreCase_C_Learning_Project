23ed6fa959802e8e5309c121ea68158cb793dab7
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 15:02:31 +0800

    3.2.4

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -482,3 +482,68 @@
 //
 //    return 0;
 //}
+
+
+//*******************************************************************
+//3.2.4
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//    float mon = 0, inter;
+//    char i;
+//    scanf("%f", &inter);
+//
+//    if (inter < 100000)
+//    {
+//        i = 1;
+//    }
+//    else if (inter <= 200000 && inter > 100000)
+//    {
+//        i = 2;
+//    }
+//    else if (inter <= 400000 && inter > 200000)
+//    {
+//        i = 3;
+//    }
+//    else if (inter <= 600000 && inter > 400000)
+//    {
+//        i = 4;
+//    }
+//    else if (inter <= 1000000 && inter > 600000)
+//    {
+//        i = 5;
+//    }
+//    else if (inter > 1000000)
+//    {
+//        i = 6;
+//    }
+//
+//    switch (i)
+//    {
+//
+//    case 6:
+//        mon = (inter - 1e6) * 0.01;
+//        inter = 1e6;
+//    case 5:
+//        mon = (inter - 6e5) * 0.015 + mon;
+//        inter = 6e5;
+//    case 4:
+//        mon = (inter - 4e5) * 0.03 + mon;
+//        inter = 4e5;
+//    case 3:
+//        mon = (inter - 2e5) * 0.05 + mon;
+//        inter = 2e5;
+//    case 2:
+//        mon = (inter - 1e5) * 0.075 + mon;
+//        inter = 1e5;
+//    case 1:
+//        mon = inter * 0.1 + mon;
+//    }
+//
+//    printf("½±½ðÊÇ%.2f", mon);
+//    return 0;
+//}
+//
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -4,17 +4,58 @@
 
 int main()
 {
-    float x, y;
-    scanf("%f,%f", &x, &y);
-    if (fabs(fabs(x) - 2) < 1 && fabs(fabs(y) - 2) < 1)
+    float mon=0,inter;
+    char i;
+    scanf("%f", &inter);
+
+    if (inter < 100000)
     {
-        printf("该点高度为10米");
+        i = 1;
     }
-    else
+    else if (inter <= 200000 && inter > 100000)
     {
-        printf("该点高度为0米");
+        i = 2;
+    }
+    else if (inter <= 400000 && inter > 200000)
+    {
+        i = 3;
+    }
+    else if (inter <= 600000 && inter > 400000)
+    {
+        i = 4;
+    }
+    else if (inter <= 1000000 && inter > 600000)
+    {
+        i = 5;
+    }
+    else if (inter > 1000000)
+    {
+        i = 6;
     }
 
+    switch (i)
+    {
+    
+    case 6:
+        mon = (inter - 1e6) * 0.01;
+        inter = 1e6;
+    case 5:
+        mon = (inter - 6e5) * 0.015 + mon;
+        inter = 6e5;
+    case 4:
+        mon = (inter - 4e5) * 0.03 + mon;
+        inter = 4e5;
+    case 3:
+        mon = (inter - 2e5) * 0.05 + mon;
+        inter = 2e5;
+    case 2:
+        mon = (inter - 1e5) * 0.075 + mon;
+        inter = 1e5;
+    case 1:
+        mon = inter * 0.1 + mon;
+    }
+
+    printf("奖金是%.2f", mon);
     return 0;
 }
     
