6ccafa42b4ace41c9c568a24e49ee5432b2ce393
FibreCase Xu <fibrecase@outlook.com>
2023-10-18 16:01:05 +0800

    4.1.3

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -641,3 +641,47 @@
 //        return n;
 //    }
 //}
+
+
+//****************************************************************
+//4.1.3
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <math.h>
+//
+//int main()
+//{
+//	int a = 1, b = 0, c = 0;
+//	int verf(int, int, int);
+//
+//	while (a < 10)
+//	{
+//		b = 0;
+//		while (b < 10)
+//		{
+//			c = 0;
+//			while (c < 10)
+//			{
+//				verf(a, b, c);
+//				c++;
+//			}
+//			b++;
+//		}
+//		a++;
+//	}
+//
+//	return 0;
+//}
+//
+//int verf(int a, int b, int c)
+//{
+//	int n, v;
+//	n = a * 100 + b * 10 + c;
+//	v = a * a * a + b * b * b + c * c * c;
+//	if (n == v)
+//	{
+//		printf("%5d", n);
+//	}
+//
+//	return 0;
+//}
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -4,60 +4,37 @@
 
 int main() 
 {
-    int m, n, i, a;
-    int min(int,int), max(int,int);
-    scanf("%d,%d", &m, &n);
-    i = min(m, n);
-    while (i>=1)
-    {
-        if (m%i==0 && n%i==0)
-        {
-            printf("最大公约数是%d\n", i);
-            break;
-        }
-        else
-        {
-            i--;
-        }
-    }
-    a = max(m, n);
-    while (1)
-    {
-        if (a%m == 0 && a % n == 0)
-        {
-            printf("最小公倍数是%d", a);
-            break;
-        }
-        else
-        {
-            a++;
-        }
-    }
+	int a=1, b=0, c=0;
+	int verf(int, int, int);
 
+	while (a < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				verf(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
 
     return 0;
 }
 
-int min(int m, int n)
+int verf(int a, int b, int c)
 {
-    if (m >= n)
-    {
-        return n;
-    }
-    else
-    {
-        return m;
-    }
-}
+	int n, v;
+	n = a * 100 + b * 10 + c;
+	v = a * a * a + b * b * b + c * c * c;
+	if (n ==v)
+	{
+		printf("%5d", n);
+	}
 
-int max(int m, int n)
-{
-    if (m >= n)
-    {
-        return m;
-    }
-    else
-    {
-        return n;
-    }
+	return 0;
 }
