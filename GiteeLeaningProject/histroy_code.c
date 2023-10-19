b9d1fcc3c367612d12952732d3c240fc77fbfddb
FibreCase Xu <fibrecase@outlook.com>
2023-10-19 21:34:00 +0800

    bugfix

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -12,15 +12,341 @@ int main()
 		m = n;
 		n = i;
 	}
-	r = m % n;
-	while (r != 0)
+
+	printf("1+2+...+%d=%d\n", n, sum);
+	return 0;
+}
+
+//************************************************************************
+//4.1.7
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int b = 0, c = 0;
+	int ver(int, int);
+	while (b < 9 && c < 9)
 	{
-		m = n;
-		n = r;
-		r = m % n;
+		for (int i = 0; i < 10;)
+		{
+			if (ver(b, c) == 1)
+				//这里使用子函数的目的是便于检测是否输出，便于输出的计数
+			{
+				i++; c++;
+				if (c == 10)
+				{
+					b++; c = 0;
+				}
+
+			}
+			else
+			{
+				c++;
+				if (c == 10)
+				{
+					b++; c = 0;
+				}
+			}
+		}
+		printf("\n");
 	}
-	y = x / n;
-	printf("最大公约数是%d", n);
-	printf("最小公倍数是%d", y);
+	return 0;
+}
+
+int ver(int b, int c)
+{
+	if ((100 + b * 10 + c) % 2 == 0 && b != c && b != 1 && c != 1)
+	{
+		printf("%6d", (100 + b * 10 + c));
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+//************************************************************************
+//4.1.6
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int n, s = 1;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		s = i * s;
+	}
+
+	printf("%d", s);
+	return 0;
+}
+
+//************************************************************************
+//4.1.5
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a = 0, e = 0, i = 0, o = 0, u = 0;
+	char c;
+
+	while ((c = getchar()) != '#')
+	{
+		if (c == 'a')
+		{
+			a++;
+		}
+		else if (c == 'e')
+		{
+			e++;
+		}
+		else if (c == 'i')
+		{
+			i++;
+		}
+		else if (c == 'o')
+		{
+			o++;
+		}
+		else if (c == 'u')
+		{
+			u++;
+		}
+	}
+
+	printf("a:%d\ne:%d\ni:%d\no:%d\nu:%d", a, e, i, o, u);
+
+	return 0;
+}
+
+//************************************************************************
+//4.1.4
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int i, j = 1;
+
+	for (i = 0; i < 9; i++)
+	{
+		j = (j + 1) * 2;
+	}
+	printf("猴子第一天摘了%d个桃子", j);
+
+	return 0;
+}
+
+
+//****************************************************************
+//4.1.3
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a = 1, b = 0, c = 0;
+	int verf(int, int, int);
+
+	while (a < 10)
+	{
+		b = 0;
+		while (b < 10)
+		{
+			c = 0;
+			while (c < 10)
+			{
+				verf(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+
+	return 0;
+}
+
+int verf(int a, int b, int c)
+{
+	int n, v;
+	n = a * 100 + b * 10 + c;
+	v = a * a * a + b * b * b + c * c * c;
+	if (n == v)
+	{
+		printf("%5d", n);
+	}
+
+	return 0;
+}
+
+
+//****************************************************
+//4.1.2
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int m, n, i, a;
+	int min(int, int), max(int, int);
+	scanf("%d,%d", &m, &n);
+	i = min(m, n);
+	while (i >= 1)
+	{
+		if (m % i == 0 && n % i == 0)
+		{
+			printf("最大公约数是%d\n", i);
+			break;
+		}
+		else
+		{
+			i--;
+		}
+	}
+	a = max(m, n);
+	while (1)
+	{
+		if (a % m == 0 && a % n == 0)
+		{
+			printf("最小公倍数是%d", a);
+			break;
+		}
+		else
+		{
+			a++;
+		}
+	}
+
+	return 0;
+}
+
+int min(int m, int n)
+{
+	if (m >= n)
+	{
+		return n;
+	}
+	else
+	{
+		return m;
+	}
+}
+
+int max(int m, int n)
+{
+	if (m >= n)
+	{
+		return m;
+	}
+	else
+	{
+		return n;
+	}
+}
+
+//***********************************************
+//4.1.1
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+	char c; //用户输入字符
+	int letters = 0, // 字母数目
+		space = 0, // 空格数目
+		digit = 0, // 整数数目
+		others = 0; // 其他字符数目
+	while ((c = getchar()) != '\n')
+	{ // 每次读取一个字符，回车时结束
+		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+			letters++;//字母＋1
+		else if (c == ' ')
+			space++;//空格+1
+		else if (c >= '0' && c <= '9')
+			digit++;//数字+1
+		else
+			others++;//其他+1
+	}
+	printf("字母数:%d\n空格数:%d\n数字数:%d\n其它字符数:%d\n\n", letters, space, digit, others);
+
+	return 0;
+}
+
+//*******************************************************************
+//3.2.4
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	float mon = 0, inter;
+	char i;
+	scanf("%f", &inter);
+
+	if (inter < 100000)
+	{
+		i = 1;
+	}
+	else if (inter <= 200000 && inter > 100000)
+	{
+		i = 2;
+	}
+	else if (inter <= 400000 && inter > 200000)
+	{
+		i = 3;
+	}
+	else if (inter <= 600000 && inter > 400000)
+	{
+		i = 4;
+	}
+	else if (inter <= 1000000 && inter > 600000)
+	{
+		i = 5;
+	}
+	else if (inter > 1000000)
+	{
+		i = 6;
+	}
+
+	switch (i)
+	{
+
+	case 6:
+		mon = (inter - 1e6) * 0.01;
+		inter = 1e6;
+	case 5:
+		mon = (inter - 6e5) * 0.015 + mon;
+		inter = 6e5;
+	case 4:
+		mon = (inter - 4e5) * 0.03 + mon;
+		inter = 4e5;
+	case 3:
+		mon = (inter - 2e5) * 0.05 + mon;
+		inter = 2e5;
+	case 2:
+		mon = (inter - 1e5) * 0.075 + mon;
+		inter = 1e5;
+	case 1:
+		mon = inter * 0.1 + mon;
+	}
+
+	printf("奖金是%.2f", mon);
+		mon = inter * 0.1 + mon;
+	}
+
+	printf("奖金是%.2f", mon);
 	return 0;
 }
