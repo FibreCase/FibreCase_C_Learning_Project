a3cada496b6eebd12d0ea411561f9fa490fbf7d7
FibreCase Xu <fibrecase@outlook.com>
2023-10-25 21:56:01 +0800

    4.2.12

--- GiteeLeaningProject/histroy_code.c
+++ GiteeLeaningProject/histroy_code.c
@@ -1,3 +1,24 @@
+//4.2.12
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int a, n, c = 0, s = 0;
+	scanf("%d%d", &a, &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		c = c + pow(10, i);
+		s = s + c * a;
+	}
+
+	printf("S%d(%d)=%d", n, a, s);
+
+	return 0;
+}
+
 //4.2.11
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
--- GiteeLeaningProject/main.c
+++ GiteeLeaningProject/main.c
@@ -4,8 +4,16 @@
 
 int main()
 {
-	int a, n;
+	int a, n, c = 0, s = 0;
+	scanf("%d%d", &a, &n);
 
+	for (int i = 0; i < n; i++)
+	{
+		c = c + pow(10, i);
+		s = s + c * a;
+	}
+
+	printf("S%d(%d)=%d", n, a, s);
 
 	return 0;
 }
